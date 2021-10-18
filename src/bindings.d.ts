c7928b3eae3d0c50059c42fb02d97074b573caa8
Felix Kleinekathoefer <git@felixoi.com>
2021-10-18 22:01:41 +0200

    Docs for the bindings

--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -1,10 +1,31 @@
 export {};
 
+/**
+ * Bindings for the cloudflare worker secrets used in the code.
+ */
 declare global {
-    const IMGUR_REFRESH_TOKEN: string;
+    /**
+     * Id of the registered imgur client.
+     */
     const IMGUR_CLIENT_ID: string;
+    /**
+     * Secret for the registered imgur client.
+     */
     const IMGUR_CLIENT_SECRET: string;
+    /**
+     * Refresh token for the registered imgur client.
+     */
+    const IMGUR_REFRESH_TOKEN: string;
+    /**
+     * Id of the registered discord application.
+     */
     const DISCORD_APPLICATION_ID: string;
+    /**
+     * Secret for the registered discord application.
+     */
     const DISCORD_APPLICATION_SECRET: string;
+    /**
+     * Public key of the registered discord application.
+     */
     const DISCORD_PUBLIC_KEY: string;
 }
